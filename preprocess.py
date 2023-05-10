071d3375a79719c472999bfc92d83574c9180eb3
Akhil Dhavala <akhil.dhavala@gmail.com>
2023-05-09 20:40:55 -0700

    test

--- preprocess.py
+++ preprocess.py
@@ -173,10 +173,14 @@ class PreProcessVideos:
             os.walk(self.video_directory),
             desc=f"Processing videos in {self.video_directory}"
         ):
+            i = 0
             for video in files:
                 if video.endswith(self.vid_types):
                     video_path = f"{self.video_directory}/{video}"
                     videos.append(video_path)
+                if i > 10:
+                    break
+                i += 1
 
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             futures = []
